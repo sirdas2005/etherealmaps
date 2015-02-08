c2889cc434e793f0c0a1a93802702bcd3c189100
sirdas2005 <sirdas2005@hotmail.com>
2015-02-09 00:08:23 +0100

    etherealmaps has born!!! 2015-02-09

---
+++ etherealmaps/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etherealmaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("etherealmaps has born!!! 2015-02-09");
+            Console.ReadKey();
+        }
+    }
+}
